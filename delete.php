e518278745016d46e27868cbcff98e74031ba1ec
Nabiha0769 <nabihazareen29@gmail.com>
2025-05-20 08:34:09 +0500

    9 files delete

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'components/connnection.php';
-$id = $_GET['id'];
-$query = "delete from tbl_products where p_id='$id'";
-$result = mysqli_query($conn,$query);
-if($result){
-    header("location:products.php");
-}
-?>
